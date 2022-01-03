b5e930e601d4d34ae163e238541a9a4184cff719
delphython <delphython@gmail.com>
2022-01-03 12:19:38 +0300

    Add dvmn api settings

--- dvmnbot.py
+++ dvmnbot.py
@@ -1,12 +1,21 @@
+import os
+
 import requests
 
-
-url = "https://yandex.ru"
-requests.get(url)
+from dotenv import load_dotenv
 
 
 def main():
-    pass
+    load_dotenv()
+    DVMN_TOKEN = os.getenv("DVMN_TOKEN")
+    dvmn_reviews_url = "https://dvmn.org/api/user_reviews/"
+
+    headers = {
+        "Authorization": f"Token {DVMN_TOKEN}",
+    }
+
+    response = requests.get(dvmn_reviews_url, headers=headers)
+    response.raise_for_status()
 
 
 if __name__ == "__main__":
