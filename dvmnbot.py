c34dae62185de01dca417ca7db4f1116b26385fb
delphython <delphython@gmail.com>
2022-01-03 13:10:56 +0300

    Add exceptions

--- dvmnbot.py
+++ dvmnbot.py
@@ -1,5 +1,7 @@
 import os
 
+from pprint import pprint
+
 import requests
 
 from dotenv import load_dotenv
@@ -8,14 +10,24 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
     DVMN_TOKEN = os.getenv("DVMN_TOKEN")
-    dvmn_reviews_url = "https://dvmn.org/api/user_reviews/"
+    dvmn_reviews_url = "https://dvmn.org/api/long_polling/"
 
     headers = {
         "Authorization": f"Token {DVMN_TOKEN}",
     }
 
-    response = requests.get(dvmn_reviews_url, headers=headers)
-    response.raise_for_status()
+    while True:
+        try:
+            response = requests.get(
+                dvmn_reviews_url, headers=headers, timeout=5
+            )
+            response.raise_for_status()
+
+            pprint(response.json())
+        except requests.exceptions.ReadTimeout:
+            pass
+        except requests.exceptions.ConnectionError:
+            pass
 
 
 if __name__ == "__main__":
