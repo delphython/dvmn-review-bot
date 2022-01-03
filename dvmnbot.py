946d4cbd49bfe76a64e277f0cf6ddeb110d095f4
delphython <delphython@gmail.com>
2022-01-03 15:49:58 +0300

    Use dvmn API timestamp for request

--- dvmnbot.py
+++ dvmnbot.py
@@ -7,23 +7,37 @@ import requests
 from dotenv import load_dotenv
 
 
+def get_dvmn_api_responce(url, token, timestamp):
+    headers = {
+        "Authorization": f"Token {token}",
+        "timestamp": timestamp,
+    }
+
+    response = requests.get(
+        url,
+        headers=headers,
+    )
+
+    response.raise_for_status()
+
+    return response.json()
+
+
 def main():
     load_dotenv()
+
     DVMN_TOKEN = os.getenv("DVMN_TOKEN")
     dvmn_reviews_url = "https://dvmn.org/api/long_polling/"
-
-    headers = {
-        "Authorization": f"Token {DVMN_TOKEN}",
-    }
+    timestamp = None
 
     while True:
         try:
-            response = requests.get(
-                dvmn_reviews_url, headers=headers, timeout=5
+            dvmn_api_response = get_dvmn_api_responce(
+                dvmn_reviews_url, DVMN_TOKEN, timestamp
             )
-            response.raise_for_status()
-
-            pprint(response.json())
+            if dvmn_api_response["status"] == "timeout":
+                timestamp = str(dvmn_api_response["timestamp_to_request"])
+            pprint(dvmn_api_response)
         except requests.exceptions.ReadTimeout:
             pass
         except requests.exceptions.ConnectionError:
