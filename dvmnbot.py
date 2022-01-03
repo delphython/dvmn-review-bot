150043809d8e5f7e347f0ecbc405fc618890037c
delphython <delphython@gmail.com>
2022-01-03 16:05:11 +0300

    Add telegram bot minimum

--- dvmnbot.py
+++ dvmnbot.py
@@ -1,8 +1,7 @@
 import os
 
-from pprint import pprint
-
 import requests
+import telegram
 
 from dotenv import load_dotenv
 
@@ -27,21 +26,28 @@ def main():
     load_dotenv()
 
     DVMN_TOKEN = os.getenv("DVMN_TOKEN")
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
     dvmn_reviews_url = "https://dvmn.org/api/long_polling/"
     timestamp = None
+    chat_id = 221413677
 
-    while True:
-        try:
-            dvmn_api_response = get_dvmn_api_responce(
-                dvmn_reviews_url, DVMN_TOKEN, timestamp
-            )
-            if dvmn_api_response["status"] == "timeout":
-                timestamp = str(dvmn_api_response["timestamp_to_request"])
-            pprint(dvmn_api_response)
-        except requests.exceptions.ReadTimeout:
-            pass
-        except requests.exceptions.ConnectionError:
-            pass
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    bot.send_message(text="Hello, Виталий", chat_id=chat_id)
+
+    # updates = bot.get_updates()
+    # print(updates[0])
+
+    # while True:
+    #     try:
+    #         dvmn_api_response = get_dvmn_api_responce(
+    #             dvmn_reviews_url, DVMN_TOKEN, timestamp
+    #         )
+    #         if dvmn_api_response["status"] == "timeout":
+    #             timestamp = str(dvmn_api_response["timestamp_to_request"])
+    #     except requests.exceptions.ReadTimeout:
+    #         pass
+    #     except requests.exceptions.ConnectionError:
+    #         pass
 
 
 if __name__ == "__main__":
